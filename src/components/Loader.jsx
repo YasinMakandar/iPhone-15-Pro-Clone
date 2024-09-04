76ebd6ec8dabd1287cfea4fcaa7b60d83a031b86
YasinMakandar <yasinmakandar03@gmail.com>
2024-09-04 17:52:09 +0530

    Added Files.

---
+++ src/components/Loader.jsx
@@ -0,0 +1,19 @@
+import { Html } from '@react-three/drei'
+import React from 'react'
+import { SyncLoader } from 'react-spinners'
+
+
+const Loader = () => {
+  return (
+    <Html>
+        <div className='absolute top-0 left-0 w-full h-full flex justify-center items-center'>
+            <div className='w-[10vw] h-[10vw] rounded-full flex'>
+               
+            <SyncLoader color="#ffffff" />
+            </div>
+        </div>
+    </Html>
+  )
+}
+
+export default Loader
